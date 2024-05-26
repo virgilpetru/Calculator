aa30ba11039b3906a02d2bc035c57203704bec69
virgilpetru <156107820+virgilpetru@users.noreply.github.com>
2024-05-26 20:39:42 +0200

    Calculator
    Python project - Calculator

---
+++ Calculator.py
@@ -0,0 +1,53 @@
+from calculator_art import logo
+def add(n1, n2):
+    return n1 + n2
+
+#Substract
+def subtract(n1, n2):
+    return n1 - n2
+
+# Multiply
+def multiply(n1, n2):
+    return n1 * n2
+
+# Divide
+def divide(n1, n2):
+    return n1 / n2
+
+operations ={
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/":divide
+}
+#function = operations["*"]
+def calculator():
+    print(logo)
+
+    num1 = (float(input("What's the first number?: ")))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+    #if operation_symbol == "+":
+    #    answer = (num1 + num2)
+    #elif operation_symbol == "-":
+    #    answer = (num2 - num1)
+    #elif operation_symbol == "*":
+    #    answer = (num1 * num2)
+    #elif operation_symbol == "/":
+    #    answer = (num1 / num2)
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation .:") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+calculator()
